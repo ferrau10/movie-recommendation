19d1090b76a206158dc302e5b379247580cd5177
mscschliemann <mschliemann@outlook.com>
2020-10-17 10:59:51 +0200

    added paging

--- application.py
+++ application.py
@@ -6,12 +6,9 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-    #movie_rec = data.random_recommander(3)
-    #print(movie_rec)
-    titles, images = data.get_images()
+    titles, images, genres = data.get_movies()
     tags = data.get_popular_tags()
-    #print(titles)
-    return render_template('index.html', movies=zip(titles, images), tags=tags)
+    return render_template('index.html', movies=list(zip(titles, images)), tags=tags)
     #return render_template('index.html')
 
 @app.route('/recommend', methods = ['POST', 'GET'])      
@@ -19,15 +16,15 @@ def recommend():
     print(request.args['userid'])
     #fname = 'John'
     #print(request.form['fname'])
-    #return render_template('index.html', text=request.form['fname'])
     return render_template('recommend.html')
 
 # trigger hello function every time somebody visits \home on my website
 @app.route('/filter')
 def filter():
-    #movie_rec = data.random_recommander(3)
-    movie_rec = ['A', 'B', 'C']
-    return render_template('index.html', from_filter=1)
+    filter = request.args['filter']
+    titles, images, genres = data.get_movies_by_genre(filter)
+    tags = data.get_popular_tags()
+    return render_template('index.html', movies=list(zip(titles, images, genres)), tags=tags, from_filter=1)
 
 @app.route('/images')
 def images():
